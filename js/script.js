f3419eea540609b3059da284c8583cf995d2329e
madep <mahfudade@gmail.com>
2024-02-10 21:14:10 +0700

    Fix ssl error (2)

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function searchMovie() {
         dataType: "json",
         data: {
             apikey: "4ec4954d",
-            s: $("#search-input").val().append("/"),
+            s: $("#search-input").val(),
         },
         success: function (result) {
             if (result.Response == "True") {
